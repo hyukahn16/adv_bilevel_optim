8908ca89e67fc3bb36d7a34a2444f6e0d234d28d
Hyuk Ahn <hyukahn@umich.edu>
2024-07-10 01:02:48 +0900

    Implement load and save model

---
+++ util.py
@@ -0,0 +1,20 @@
+import torch
+import os
+
+def save_model(model, epoch, optimizer, save_dir):
+    save_state = {
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        # 'optimizer_state_dict': self.optimizer.state_dict(),
+    }
+    torch.save(save_state, save_dir + "/ckpt_{}.pt".format(epoch))
+    print("Model saved at epoch {}".format(epoch))
+
+def load_model(load_dir, model, epoch):
+    load_dir = os.path.join(load_dir, "ckpt_{}.pt".format(epoch))
+    checkpoint = torch.load(load_dir)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    print("Loaded model from {}".format(load_dir))
+    return epoch
