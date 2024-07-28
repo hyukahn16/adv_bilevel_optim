a56477baf35b3d3fe219691441c10cf5591bcb7b
Hyuk Ahn <hyukahn@umich.edu>
2024-07-29 00:49:41 +0900

    Cleanup

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
-from multiprocessing import Process
-
 # import cProfile
 
 from models import *
@@ -19,21 +16,22 @@ if __name__ == "__main__":
     torch.backends.cudnn.benchmark = True
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = ResNet18().to(device)
-    trainLoader = get_trainloader(trainBatch=512)
+    trainLoader = get_trainloader(trainBatch=500)
     testLoader = get_testloader(testBatch=400, shuffle=False)
     pgdAdv = PGD(model)
 
     # Define hyperparameters
     eps = 8/255
-    lr = 0.1 # Default 0.1 (used 0.05)
+    lr = 0.001 # Default 0.1 (used 0.05)
     atkIter = 10
     trainEpochStart, trainEpochEnd = 0, 50
     save = True
-    load = False
-    loadEpoch = 50 # Only used if load == True
+    load = True
+    test = True
+    trainPGD = True
+    loadEpoch = 100 # Only used if load == True, use ckpt_num
     saveDir = "saved_models"
-    saveDir = os.path.join(saveDir, "new_bilevel")
-    trainPGD = False
+    saveDir = os.path.join(saveDir, "new_pgd")
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(
@@ -46,8 +44,9 @@ if __name__ == "__main__":
     if load:
         load_model(saveDir, model, loadEpoch)
         trainEpochStart = loadEpoch
-        trainEpochEnd += trainEpochStart + 50
+        trainEpochEnd = trainEpochStart + 50
         saveDir += "_" + str(trainEpochStart)
+        saveDir += "_" + str("100_001")
     if save or load:
         if os.path.isdir(saveDir):
             exit("saveDir already exists! Exiting...")
@@ -67,30 +66,23 @@ if __name__ == "__main__":
             trainPGD
             )
         
-        if e % 1 == 0:
+        if test and e % 1 == 0:
             print("\nTest Epoch: {}".format(e))
             pgd_test(
-                model, testLoader, 
+                model, testLoader,
                 criterion, pgdAdv, logger,
                 # stopIter=25
                 )
  
-        if save and (e+1) % 10 == 0:
+        if save and (e+1) % 5 == 0:
             print("\nSave Epoch: {}".format(e))
             save_model(model, e+1, optimizer, saveDir)
 
-        if e == 99:
-            print("\nChanged learning rate to 0.005\n")
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = lr / 10
-
-    # print("\nLAST TEST")
-    # pgd_test(model, testLoader, criterion, pgdAdv)
+        # if e == 100:
+        #     for param_group in optimizer.param_groups:
+        #         param_group['lr'] = lr / 10
+        #     print("\nChanged learning rate to {}\n".format(lr/10))
 
     # if save:
     #     print("\nSave Epoch: {}".format(trainEpochEnd-1))
     #     save_model(model, trainEpochEnd-1, optimizer, saveDir)
-
-    # plotter = Plot(saveDir, logger)
-    # plotter.draw_figure_losses()
-    # plotter.draw_figure_margins()
